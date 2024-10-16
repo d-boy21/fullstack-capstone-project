81937bf6d5507faa9f9ff231da6018cddcaa04e4
NIck <d-boy21@hotmail.com>
2024-10-16 10:25:34 -0400

    ADded details page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MainPage from './components/MainPage/MainPage';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function App() {
 
@@ -15,6 +15,7 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
